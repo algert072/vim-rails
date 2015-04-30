algert072/vim-rails

commit 31298088d888b87c235eab9239ff6124c46c8331

De-dupe autocomplete results
Duplicates appear when overriding a default command via a projection

autoload/rails.vim
@@ -2537,7 +2537,7 @@ function! s:completion_filter(results, A, ...) abort
   if exists('*projectionist#completion_filter')
     return projectionist#completion_filter(a:results, a:A, a:0 ? a:1 : '/')
   endif
-  let results = sort(type(a:results) == type("") ? split(a:results,"\n") : copy(a:results))
+  let results = uniq(sort(type(a:results) == type("") ? split(a:results,"\n") : copy(a:results)))
   call filter(results,'v:val !~# "\\~$"')
   if a:A =~# '\*'
     let regex = s:gsub(a:A,'\*','.*')

commit 09e61ba8eb153a5000fbcc4d592f7ba57bf4e859

trivial bugfix - use s:uniq() not uniq()
Bug introduced in 31298088d888b87c235eab9239ff6124c46c8331.

Causes older versions of vim pre the uniq function to projectile vomit errors into your eyes when tab completing.

autoload/rails.vim
@@ -2537,7 +2537,7 @@ function! s:completion_filter(results, A, ...) abort
   if exists('*projectionist#completion_filter')
     return projectionist#completion_filter(a:results, a:A, a:0 ? a:1 : '/')
   endif
-  let results = uniq(sort(type(a:results) == type("") ? split(a:results,"\n") : copy(a:results)))
+  let results = s:uniq(sort(type(a:results) == type("") ? split(a:results,"\n") : copy(a:results)))
   call filter(results,'v:val !~# "\\~$"')
   if a:A =~# '\*'
     let regex = s:gsub(a:A,'\*','.*')